75d7e037a06cf2a56525478c369f4627ba63c3dc
Yifei Wu <yifeiwublazer@gmail.com>
2019-08-25 00:18:08 +0200

    refactor routes, clean up the app.js file

---
+++ v5/routes/campgrounds.js
@@ -0,0 +1,49 @@
+var express    = require("express");
+var router     = express.Router();
+var Campground = require("../models/campground");
+
+router.get("/", (req, res) => {
+	// Get all campgrounds from DB
+	Campground.find({}, (err, allCampgrounds) => {
+		if(err){
+			console.log("Error");
+			console.log(err);
+		} else {
+			res.render("campgrounds/index", {campgrounds: allCampgrounds});
+		}
+	});
+});
+
+router.post("/", (req, res) => {
+    var name = req.body.name;
+	var image = req.body.image;
+	var description = req.body.description;
+    var newCampground = {name: name, image: image, description: description};
+	// Create a new campground and save to DB
+	Campground.create(newCampground, (err, newlyCreated) => {
+		if(err){
+			console.log(err);
+		} else {
+			res.redirect("/campgrounds");
+		}
+	});
+	// campgrounds.push(newCampground);
+});
+
+router.get("/new", (req, res) => {
+    res.render("campgrounds/new");
+});
+
+router.get("/:id", (req, res) => {
+	// find the campground with provied ID
+	Campground.findById(req.params.id).populate("comments").exec((err, foundCampground) => {
+		if(err){
+			console.log(err);
+		} else {
+			// render show template with that campground
+			res.render("campgrounds/show", {campground: foundCampground});
+		}
+	});
+});
+
+module.exports = router;
